b0ed11fa3e46bed08697e6839375862933afb572
Luiz Felipe <95968468+Luiz9910@users.noreply.github.com>
2023-03-19 23:08:54 -0300

    favicon adicionado

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const router = require("./routes/routes")
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
+app.use("/favicon.ico", express.static("src/images/favicon.ico"))
 app.use(bodyParser.json())
-
 app.use("/",router);
 
 app.listen(8686,() => {
---
+++ teste.js
@@ -0,0 +1,4 @@
+let array = [1,2,3]
+
+let opa = array.find(a => a == 1)
+console.log(opa)
